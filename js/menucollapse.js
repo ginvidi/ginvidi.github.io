241a9b4bd01c1b8f6af1fbdd2ddd4455980a9c5b
Ginevra <ginvidi@gmail.com>
2014-12-03 15:14:58 +0100

    mobile version menu fix in the corner

--- js/menucollapse.js
+++ js/menucollapse.js
@@ -8,27 +8,34 @@ var menu;
 			$('#menuList').css('display', 'block');
 			 menu=true;
 
+
+
+		/*mouse over menues items*/
+	
+
+		     $('#menuList li a').hover(
+			 function () {
+			   $(this).parent().css({"background-color":"#eeeeee"});
+					$( this ).click(function() {
+						$('#menuList').css('display', 'none');
+						menu=false;
+					});
+			 }, 
+			 function () {
+			   $(this).parent().css({"background-color":"#ffffff"});
+			 }
+		     );
+
 		}
 		else{
 			$('#menuList').css('display', 'none');
 			 menu=false;
 
+
 		}
-			
+
 
 	});
-
-/*mouse over menues items*/
-	
-
-     $('#menuList li a').hover(
-         function () {
-           $(this).parent().css({"background-color":"#eeeeee"});
-         }, 
-         function () {
-           $(this).parent().css({"background-color":"#ffffff"});
-         }
-     );
 
 });
 
